b1afae0e454b289b7d956b0a8af0d89dbdc8693b
linsadry <adriana_lins@hotmail.com>
2026-05-13 20:03:49 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SB_URL = import.meta.env.VITE_SUPABASE_URL || 'https://unjbdcjcfqmytapxyvuf.supabase.co'
-const SB_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuamJkY2pjZnFteXRhcHh5dnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODUzNDAsImV4cCI6MjA2MTM2MTM0MH0.j4VQfPNvAyMOkN7F3eiGSBzSV-WBzZ_XRhNjOY8bT_E'
+const SB_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuamJkY2pjZnFteXRhcHh5dnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTQ1NDAsImV4cCI6MjA5MjI5MDU0MH0.Wvc2IaYUib3zO1b92tAd5d9pH4Tpzr6cMm9yGKEelKs'
 
 export const supabase = createClient(SB_URL, SB_KEY)
