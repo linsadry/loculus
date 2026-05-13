4da0e4161e1d838750cda30866388bf6e6526a68
linsadry <adriana_lins@hotmail.com>
2026-05-13 20:34:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState, useRef, useEffect, useCallback } from "react";
-import { createClient } from '@supabase/supabase-js';
-const supabase = createClient(
-  'https://unjbdcjcfqmytapxyvuf.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuamJkY2pjZnFteXRhcHh5dnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODUzNDAsImV4cCI6MjA2MTM2MTM0MH0.j4VQfPNvAyMOkN7F3eiGSBzSV-WBzZ_XRhNjOY8bT_E'
-);
+import { supabase } from "./lib/supabase";
 
 const IMG_URL = "https://unjbdcjcfqmytapxyvuf.supabase.co/storage/v1/object/public/assets/IMG_8652.png";
 
